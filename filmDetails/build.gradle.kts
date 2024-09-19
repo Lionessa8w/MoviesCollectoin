b90fc2ef7775e7ba4f4872a244b76f48c8bcb57c
Marina <marina8ww1@gmail.com>
2024-09-19 12:42:40 +0700

    добавление модулей

--- filmDetails/build.gradle.kts
+++ filmDetails/build.gradle.kts
@@ -7,3 +7,9 @@ plugins {
 android {
     namespace = "com.example.moviescollectoin.filmDetails"
 }
+dependencies{
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+//    implementation(project(":filmsData"))
+//    implementation(project(":filmsDomain"))
+}
